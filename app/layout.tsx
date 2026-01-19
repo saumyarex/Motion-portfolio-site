7282ac5dd08c2ae3f54450b35ad15fb98ae50e95
Saumya Nayak <saumyo224@gmail.com>
2026-01-19 15:48:46 +0530

    added subsets latin

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ weight: ["400", "500", "600", "700", "800", "900"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Porfolio Website ",
