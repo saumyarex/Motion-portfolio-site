981b1825c2556b4a64b029e9173480e6f449b811
Saumya Nayak <saumyo224@gmail.com>
2026-01-27 17:51:05 +0530

    added metadata fetched from blog

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,66 @@
+import type { Metadata } from "next";
+import Container from "@/components/Container";
+import "../../globals.css";
+import { compileMDX } from "next-mdx-remote/rsc";
+
+import { getBlog, getBlogsFrontMatter } from "@/utils/mdx";
+import { redirect } from "next/navigation";
+
+// export const metadata: Metadata = {
+//   title: "Blogs by Saumya Rex",
+//   description:
+//     "Blogs by Saumya Rex, a software engineer with a passion for building scalable and efficient systems.",
+// };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const blog = await getBlogsFrontMatter(slug);
+  if (!blog) {
+    return {
+      title: "Blog not found",
+    };
+  }
+
+  return {
+    title: blog.title,
+    description: blog.description,
+  };
+}
+
+export default async function Blog({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const singleBlogPath = await getBlog(slug);
+
+  if (!singleBlogPath) {
+    redirect("/blogs");
+  }
+
+  const { content } = await compileMDX<{
+    title: string;
+    date: Date;
+    description: string;
+    tags: string[];
+    image: string;
+  }>({
+    source: singleBlogPath,
+    options: { parseFrontmatter: true },
+  });
+
+  return (
+    <div className="flex items-start justify-start">
+      <Container className="prose mt-10 min-h-screen p-4 md:p-20 md:pb-10">
+        {content}
+        {/* <MDXRemote source={singleBlogPath} /> */}
+      </Container>
+    </div>
+  );
+}
